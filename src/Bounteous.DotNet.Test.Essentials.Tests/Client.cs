178434f40c9dde3fccc0f4310d6a36c79a252fba
Greg Cook <greg.cook@nutrien.com>
2025-01-23 05:43:11 -0700

    bou-1 - triggering a build

--- src/Bounteous.DotNet.Test.Essentials.Tests/Client.cs
+++ src/Bounteous.DotNet.Test.Essentials.Tests/Client.cs
@@ -4,8 +4,7 @@ namespace Bounteous.DotNet.Test.Essentials.Tests
 {
     public class Client(IService service)
     {
-        public async Task Go()
-            => await service.Go();
+        public async Task Go() => await service.Go();
 
         public async Task Go(Request request)
             => await service.Go(request);
